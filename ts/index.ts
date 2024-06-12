824c31f70ff2bd1dbfb81a4eab8e66515f78d4c2
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2024-06-12 20:30:12 +0100

    feat: run demo on window load

--- ts/index.ts
+++ ts/index.ts
@@ -1,16 +1,20 @@
 import * as wasm from "rust_client_wasm";
 
-export function super_foo(): string {
+declare global {
+  interface Window {
+    nil: any;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+  await wasm.default();
+  console.log(super_foo());
+});
+
+function super_foo(): string {
   return `hello from rust: ${wasm.rust_foo()} and hello from ts: ${ts_hello()}`;
 }
 
 export function ts_hello(): string {
   return ":wave_in_ts:";
 }
-
-async function init() {
-  await wasm.default();
-  console.log(super_foo());
-}
-
-init();
